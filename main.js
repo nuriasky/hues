51f83fda7fff97f15c30d6b2243feb67b7e78d14
Nuriasky <nuriasky814@gmail.com>
2023-06-13 17:56:29 -0300

    first commit

--- main.js
+++ main.js
@@ -10,45 +10,57 @@ window.addEventListener("scroll", () => {
 
 const productos = [
     {
+        id: 1,
         nombre: "Pantalón Palazzo",
         precio: 12300,
+        category: "jeans",
         stock: true,
         image:"./assets/products/pantalon-palazzo.png",
     },
 
     {
+        id: 2,
         nombre: "Falda Pleated",
         precio: 8200,
+        category: "faldas",
         stock: true,
         image:"./assets/products/falda-pleated.png",
     },
 
     {
+        id: 3,
         nombre: "Top Blue",
         precio: 2000,
+        category: "camisas y tops",
         stock: true,
         image:"./assets/products/top-blue.png",
     },
 
     {
+        id: 4,
         nombre: "Falda Denim",
         precio: 18200,
+        category: "faldas",
         stock: true,
         image:"./assets/products/falda-denim.png",
     },
 
     {
+        id: 5,
         nombre: "Leather Campera Negra",
         precio: 35000,
+        category: "buzos y camperas",
         stock: true,
         image:"./assets/products/leather campera.png",
     },
-    
+
     {
-        nombre: "Leather Campera Negra",
-        precio: 35000,
+        id: 6,
+        nombre: "Blazer Beige",
+        precio: 17000,
+        category: "buzos y camperas",
         stock: true,
-        image:"./assets/products/leather campera.png",
+        image:"./assets/products/blazer-beige.png",
     },
 ]
 
